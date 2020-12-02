99e41c701a30ddac925ff63b9b7fe40cebe634a9
anathegrey <anajorgecsoaresalmeida@gmail.com>
2020-12-02 00:04:28 +0000

    review substitution

--- Typeinf.hs
+++ Typeinf.hs
@@ -4,21 +4,33 @@ module Typeinf where
        subst :: (Type, Type) -> [(Type, Type)] -> [(Type, Type)]
        subst _ [] = []
        subst (alpha, t) ((t1, t2) : ts)
-             | t1 == alpha = (alpha, t2) : (subst (alpha, t) ts)
-             | t2 == alpha = (t1, alpha) : (subst (alpha, t) ts)
-             | otherwise = (t1, t2) : (subst (alpha, t) ts)
+             | (searchType alpha ts) == True = [(NoType, NoType)]
+             | otherwise =
+                  case () of
+                    () | t1 == t   -> (alpha, t2) : (subst (alpha, t) ts)
+                       | t2 == t   -> (t1, alpha) : (subst (alpha, t) ts)
+                       | otherwise -> (t1, t2) : (subst (alpha, t) ts)
 
        unify :: [(Type, Type)] -> (Char, Int) -> [(Type, Type)] 
        unify [] _ = []
        unify (((AppT e1 e2), (AppT e3 e4)) : ts) (s, oc) = (unify ((e1, e3) : (e2, e4) : ts) (s, oc))
        unify ((t1, t2) : ts) (s, oc)
              | t1 == t2 = (unify ts (s, oc))
-             | t2 == (VarT (s : show oc)) = (unify ((t2, t1) : ts) (s, oc)) --falta o caso de alfa = t
-             | t1 == (VarT (s : show oc)) = if (searchType t2 ts) == False then (t1, t2) : (subst (t1, t2) ts) else [(NoType, NoType)]
+             | t2 == (VarT (s : show oc)) = (unify ((t2, t1) : ts) (s, oc)) 
+             | t1 == (VarT (s : show oc)) = (t1, t2) : (subst (t1, t2) ts) 
              | otherwise = (unify (ts ++ [(t1, t2)]) (s, oc))
 
-       --substBasis :: [(Type, Type)] -> [Basis] -> [Basis]
-       --substType :: [(Type, Type)] -> Type -> Type
+       substBasis :: [(Type, Type)] -> [Basis] -> [Basis]
+       substBasis [] _ = []
+       substBasis _ [] = []
+       substBasis ((t1, t2) : ts) ((Gama x tx) : gs)
+                  | tx == t2 = (Gama x t2) : (substBasis ((t1, t2) : ts) gs)
+                  | otherwise = substBasis ts ((Gama x tx) : gs)
+
+
+       {-substType :: [(Type, Type)] -> Type -> Type
+       substType ((t1, t2) : ts) alpha = if t1 == alpha then (t1, t2) : (substType ts alpha) else (t1, t2) : (substType ts alpha)
+       -}
 
        vFind :: [String] -> [Basis] -> [Basis] -> [(Type, Type)]
        vFind [] _ _ = []
@@ -28,13 +40,12 @@ module Typeinf where
        milner (VarE x) oc s = ([Gama x (VarT (s : show oc))], (VarT (s : show oc)), oc, s)
        milner (Lambda x expr) oc s = if (search g x) == False then (g, (AppT (VarT (s : show oc)) t), oc, s) else ((remove g x), AppT (VarT "t") (VarT (s : show oc)), oc, s)
                                      where (g, t, c, s1) = (milner expr (count oc) s)
-       --milner (AppE m1 m2) oc s = let v = intersect (freeVar m1) (freeVar m2)
-{-
-       algorithmT (AppE m1 m2) oc = unify ((turnSet (getType g1 v) (getType g2 v)), (Equal t1 (AppT t2 a)), oc)
-                                  where v = intersect 
-				        (g1, t1, c1) = (algorithmT m1 (count oc))
-				        (g2, t2, c2) = (algorithmT m2 (count oc))
--}
+       milner (AppE m1 m2) oc s =
+                               let v = intersect (freeVar m1) (freeVar m2)
+                                   ss = unify ((vFind v g1 g2) ++ [(t1, AppT t2 (VarT (s : (show oc))))]) (s, oc)
+                                   (g1, t1, c1, a1) = (milner m1 (count oc) s)
+                                   (g2, t2, c2, a2) = (milner m2 (count oc) 'a')
+                               in ((substBasis ss g1 ++ g2), (VarT (s : (show oc))), oc, s)
 
 
 
